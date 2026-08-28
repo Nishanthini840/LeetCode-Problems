ac9ea48a7796c139acc9df12c4ac30b0476ebc67
NISHANTHINI SANKAR <sankarnisha21@gmail.com>
2026-08-28 12:22:19 +0530

    Time: 11 ms (85.67%), Space: 12.7 MB (17.6%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def solveNQueens(self, n):
+
+        result = []
+        board = [["."] * n for _ in range(n)]
+
+        cols = set()
+        diag1 = set()
+        diag2 = set()
+
+        def backtrack(row):
+
+            if row == n:
+                solution = []
+
+                for r in board:
+                    solution.append("".join(r))
+
+                result.append(solution)
+                return
+
+            for col in range(n):
+
+                if col in cols:
+                    continue
+
+                if row - col in diag1:
+                    continue
+
+                if row + col in diag2:
+                    continue
+
+                board[row][col] = "Q"
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+
+                backtrack(row + 1)
+
+                board[row][col] = "."
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+
+        backtrack(0)
+        return result
