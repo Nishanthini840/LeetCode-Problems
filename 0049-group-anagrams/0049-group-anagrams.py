34b2bf75e7fb4885864f5d35943a7299fd616190
NISHANTHINI SANKAR <sankarnisha21@gmail.com>
2026-08-28 10:36:47 +0530

    Time: 25 ms (31.88%), Space: 17.9 MB (19.07%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+
+        groups = {}
+
+        for word in strs:
+
+            count = [0] * 26
+
+            for char in word:
+                index = ord(char) - ord('a')
+                count[index] += 1
+
+            key = tuple(count)
+
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(word)
+
+        return groups.values()
+        
