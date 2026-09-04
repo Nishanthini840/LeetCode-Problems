16441065fb967eb7d881f66234962b6c6f2702e7
NISHANTHINI SANKAR <sankarnisha21@gmail.com>
2026-09-04 20:02:34 +0530

    Time: 669 ms (76.81%), Space: 12.6 MB (25.54%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -8,40 +8,61 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
-
                 if board[r][c] == ".":
                     empty.append((r, c))
                 else:
                     num = board[r][c]
-                    rows[r].add(num)
-                    cols[c].add(num)
-                    boxes[(r // 3) * 3 + c // 3].add(num)
+                    box = (r // 3) * 3 + c // 3
 
-        def backtrack(index):
-
-            if index == len(empty):
-                return True
-
-            r, c = empty[index]
-            box = (r // 3) * 3 + c // 3
-
-            for num in "123456789":
-
-                if num not in rows[r] and num not in cols[c] and num not in boxes[box]:
-
-                    board[r][c] = num
                     rows[r].add(num)
                     cols[c].add(num)
                     boxes[box].add(num)
 
-                    if backtrack(index + 1):
-                        return True
+        def backtrack():
 
-                    board[r][c] = "."
-                    rows[r].remove(num)
-                    cols[c].remove(num)
-                    boxes[box].remove(num)
+            if not empty:
+                return True
+
+            best = -1
+            best_options = None
+
+            for i in range(len(empty)):
+
+                r, c = empty[i]
+                box = (r // 3) * 3 + c // 3
+
+                options = []
+
+                for num in "123456789":
+                    if (num not in rows[r] and
+                        num not in cols[c] and
+                        num not in boxes[box]):
+                        options.append(num)
+
+                if best_options is None or len(options) < len(best_options):
+                    best = i
+                    best_options = options
+
+            r, c = empty.pop(best)
+            box = (r // 3) * 3 + c // 3
+
+            for num in best_options:
+
+                board[r][c] = num
+                rows[r].add(num)
+                cols[c].add(num)
+                boxes[box].add(num)
+
+                if backtrack():
+                    return True
+
+                board[r][c] = "."
+                rows[r].remove(num)
+                cols[c].remove(num)
+                boxes[box].remove(num)
+
+            empty.append((r, c))
 
             return False
 
-        backtrack(0)
+        backtrack()
