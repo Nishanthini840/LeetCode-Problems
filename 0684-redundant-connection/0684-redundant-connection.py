e9312aed88ee9b645d6ca655eef19c80516a13fb
NISHANTHINI SANKAR <sankarnisha21@gmail.com>
2026-09-05 15:04:19 +0530

    Time: 0 ms (100%), Space: 12.6 MB (80.64%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findRedundantConnection(self, edges):
+        parent = list(range(len(edges) + 1))
+
+        def find(i):
+            if parent[i] == i:
+                return i
+            parent[i] = find(parent[i])  # Path compression
+            return parent[i]
+
+        for u, v in edges:
+            root_u = find(u)
+            root_v = find(v)
+
+            if root_u == root_v:
+                return [u, v]
+
+            parent[root_u] = root_v
