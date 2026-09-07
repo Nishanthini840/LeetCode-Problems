d3c220eac9af802e6a16a47cc869cd1ed4e2d711
NISHANTHINI SANKAR <sankarnisha21@gmail.com>
2026-09-07 14:51:29 +0530

    Time: 0 ms (100%), Space: 13 MB (86.86%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, pre in prerequisites:
+            graph[pre].append(course)
+            indegree[course] += 1
+
+        queue = []
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        result = []
+
+        while queue:
+            course = queue.pop(0)
+            result.append(course)
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        if len(result) == numCourses:
+            return result
+
+        return []
+        
